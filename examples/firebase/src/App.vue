9f83036d9805983e59b89ec271cfa67bf9638bee
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2022-06-16 15:22:50 +0800

    add simple main webpage layout

--- examples/firebase/src/App.vue
+++ examples/firebase/src/App.vue
@@ -1,12 +1,46 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import SignUp from './components/SignUp.vue';
+import RTDB from './components/RTDB.vue';
+
+function toggle(tabname) {
+  let content = document.getElementsByClassName("tab-content");
+
+  for (let i = 0, max = content.length; i < max; i++) {
+    content[i].style.display = "none";
+  }
+
+  document.getElementById(tabname).style.display = "block";
+  document.getElementById(`${tabname}-content`).style.display = "block";
+}
+
+window.onload = () => { toggle('auth'); }
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <img id="logo-overflow" alt="Overflow" src="https://np-overflow.club/images/full-black.png" />
+
+  <div id="auth" class="tab-content">
+    <h1><br />Authentication</h1>
+    <p>Firebase Authentication Example</p>
+  </div>
+
+  <div id="rtdb" class="tab-content">
+    <h1>Real-Time <br /> Database</h1>
+    <p>Firebase Real-Time Database Example</p>
+  </div>
+
+  <div class="tab-holder">
+    <button class="tab-link" v-on:click="toggle('auth')">Authentication</button>
+    <button class="tab-link" v-on:click="toggle('rtdb')">Real-Time Database</button>
+  </div>
+
+  <div id="auth-content" class="tab-content content">
+    <SignUp />
+  </div>
+
+  <div id="rtdb-content" class="tab-content content">
+    <RTDB />
+  </div>
 </template>
 
 <style>
@@ -16,6 +50,50 @@ import HelloWorld from './components/HelloWorld.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 1.5vh;
+}
+
+#logo-overflow {
+  width: 5%;
+  height: 5%;
+}
+
+.tab-link {
+  background-color: #000000;
+  color: #FFFFFF;
+  float: left;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  padding: 2vh 1vw;
+  font-size: 17px;
+  width: 25%;
+}
+
+.tab-link:hover {
+  background-color: #9c9c9c;
+}
+
+.tab-content {
+  color: #000000;
+  display: none;
+  text-align: center;
+}
+
+.tab-content>h1 {
+  margin: 0;
+}
+
+.tab-holder {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+}
+
+.content {
+  margin-top: 2.5vh;
 }
 </style>
