42a819302ccd4d010fb4147f7221748da82f83b5
CossetCosmi <53483420+CossetCosmi@users.noreply.github.com>
2022-06-17 01:51:01 +0800

    refactor RTDB

--- examples/firebase/src/components/RTDB.vue
+++ examples/firebase/src/components/RTDB.vue
@@ -1,197 +1,125 @@
-<script setup>
+<script>
 import { getDatabase, ref, onValue, set, remove } from "firebase/database";
 
-function insert() {
-    const name = document.getElementById('name').value;
-    const studentId = document.getElementById('studentId').value;
-    const course = document.getElementById('course').value;
+const database = getDatabase();
 
-    if ([name, studentId, course].includes('')) {
-        alert('One of the student info is null or empty');
-        return;
-    }
+export default {
+    data() {
+        return {
+            studentList: [],
+            enabledId: '',
+        }
+    },
+    watch: {
+        studentList: function () {
+            console.log(this.studentList);
+        }
+    },
+    methods: {
+        create: function () {
+            const name = this.$refs.name.value;
+            const id = this.$refs.id.value;
+            const course = this.$refs.course.value;
 
-    set(ref(getDatabase(), 'student/' + studentId), {
-        name: name,
-        studentId: studentId,
-        course: course
-    });
-}
+            if ([name, id, course].includes('')) {
+                alert('1 or more of the given detail is null or empty');
+                return;
+            }
 
-function retrieveAll() {
-    onValue(ref(getDatabase(), 'student'), (snapshot) => {
-        const studentList = snapshot.val();
-        console.log(studentList);
-        addTableRow(studentList);
-    });
-}
+            set(ref(database, `student/${id}`), {
+                name: name,
+                id: id,
+                course: course
+            });
 
-function deleteById(studentId) {
-    remove(ref(getDatabase(), `student/${studentId}`));
-}
+            this.enabledId = '';
+        },
 
-function updateById(id) {
-    const name = document.getElementById(`${id}-name`).value;
-    const studentId = document.getElementById(`${id}-studentId`).value;
-    const course = document.getElementById(`${id}-course`).value;
+        retrieveAll: function () {
+            onValue(ref(database, 'student'), (snapshot) => {
+                this.studentList = snapshot.val();
+            });
+        },
 
-    if ([name, studentId, course].includes('')) {
-        alert('One of the student info is null or empty');
-        return;
-    }
+        updateById: function (id) {
+            const newName = this.$refs[`${id}-name`][0].value;
+            const newId = this.$refs[`${id}-id`][0].value;
+            const newCourse = this.$refs[`${id}-course`][0].value;
 
-    set(ref(getDatabase(), 'student/' + studentId), {
-        name: name,
-        studentId: studentId,
-        course: course
-    });
+            if ([newName, newId, newCourse].includes('')) {
+                alert('1 or more of the given detail is null or empty');
+                return;
+            }
 
-    deleteById(id);
-}
+            set(ref(database, `student/${newId}`), {
+                name: newName,
+                id: newId,
+                course: newCourse
+            }).then(() => this.deleteById(id));
 
-// UI
-function addTableRow(studentList) {
-    // clean data
-    const existingRow = document.querySelectorAll('.student-data');
+            this.enabledId = '';
+        },
 
-    for (let i = 0, max = existingRow.length; i < max; i++) {
-        const row = existingRow[i];
-        row.remove();
-    }
+        deleteById: function (id) {
+            remove(ref(database, `student/${id}`));
+        },
 
-    // add data
-    for (let key in studentList) {
-        const student = studentList[key];
-
-        const table = document.querySelector('#student-table > tbody');
-        const row = document.createElement('tr');
-
-        row.classList.add('student-data');
-
-        const colName = document.createElement('td');
-        const colStudentId = document.createElement('td');
-        const colCourse = document.createElement('td');
-        const colAction = document.createElement('td');
-
-        // Column name, student id, course
-        colName.textContent = student['name'];
-        colStudentId.textContent = student['studentId'];
-        colCourse.textContent = student['course'];
-
-        // Column action (last column)
-        const btnUpdate = document.createElement('button');
-        const btnDelete = document.createElement('button');
-
-        btnUpdate.addEventListener('click', () => allowModify(student['studentId']));
-        btnDelete.addEventListener('click', () => deleteById(student['studentId']));
-
-        const icoEdit = document.createElement('i');
-        const icoTrash = document.createElement('i');
-
-        icoEdit.classList.add('fa');
-        icoEdit.classList.add('fa-pencil-square-o');
-
-        icoTrash.classList.add('fa');
-        icoTrash.classList.add('fa-trash-o');
-
-        btnUpdate.appendChild(icoEdit);
-        btnDelete.appendChild(icoTrash);
-
-        colAction.appendChild(btnUpdate);
-        colAction.appendChild(btnDelete);
-
-        // Add all column to table row
-        row.appendChild(colName);
-        row.appendChild(colStudentId);
-        row.appendChild(colCourse);
-        row.appendChild(colAction);
-
-        row.id = student['studentId'];
-
-        // Add row to table body
-        table.appendChild(row);
+        enableEdit: function (id) {
+            this.enabledId = id;
+        }
     }
 }
-
-function allowModify(studentId) {
-    const row = document.getElementById(studentId);
-    const name = document.querySelector(`#${studentId} > td:nth-child(1)`).textContent;
-    const course = document.querySelector(`#${studentId} > td:nth-child(3)`).textContent;
-
-    row.innerHTML = '';
-
-    const colName = document.createElement('td');
-    const colStudentId = document.createElement('td');
-    const colCourse = document.createElement('td');
-    const colAction = document.createElement('td');
-
-    const tbName = document.createElement('input');
-    tbName.setAttribute('type', 'text');
-    tbName.setAttribute('id', `${studentId}-name`);
-    tbName.setAttribute('placeholder', 'Student Name');
-    tbName.setAttribute('value', name);
-
-    const tbStudentId = document.createElement('input');
-    tbStudentId.setAttribute('type', 'text');
-    tbStudentId.setAttribute('id', `${studentId}-studentId`);
-    tbStudentId.setAttribute('placeholder', 'Student ID');
-    tbStudentId.setAttribute('value', studentId);
-
-    const tbCourse = document.createElement('input');
-    tbCourse.setAttribute('type', 'text');
-    tbCourse.setAttribute('id', `${studentId}-course`);
-    tbCourse.setAttribute('placeholder', 'Student Course');
-    tbCourse.setAttribute('value', course);
-
-    const btnUpdate = document.createElement('button');
-    btnUpdate.addEventListener('click', () => updateById(studentId));
-    btnUpdate.textContent = 'Update';
-
-    colName.appendChild(tbName);
-    colStudentId.appendChild(tbStudentId);
-    colCourse.appendChild(tbCourse);
-    colAction.appendChild(btnUpdate);
-
-    row.append(colName);
-    row.append(colStudentId);
-    row.append(colCourse);
-    row.append(colAction);
-}
-
-retrieveAll(); 
 </script>
 
 <template>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/5.5.0/css/foundation.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css" />
-    <div class="row">
-        <div class="large-12 columns">
-            <h1>CRUD Table</h1>
-            <table id="student-table">
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Student ID</th>
-                        <th>Course</th>
-                        <th></th>
-                    </tr>
-                </thead>
-                <tfoot></tfoot>
-                <tbody>
-                    <tr>
-                        <td> <input type="text" id="name" placeholder="Student Name" /> </td>
-                        <td> <input type="text" id="studentId" placeholder="Student ID" /> </td>
-                        <td> <input type="text" id="course" placeholder="Student Course" /> </td>
-                        <td><button class="" v-on:click="insert()">Submit</button></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    </div>
+
+    <input v-model="name" placeholder="Student Name" ref="name" />
+    <input v-model="id" placeholder="Student ID" ref="id" />
+    <input v-model="course" placeholder="Student Course" ref="course" />
+    <button @click="create">Create</button>
+    <button @click="retrieveAll">Fetch All</button>
+
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Student ID</th>
+                <th>Course</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="student in studentList">
+                <td>
+                    <input :ref="student['id'] + '-name'" :value="student['name']"
+                        v-bind:disabled="!(enabledId == student['id'])" />
+                </td>
+                <td>
+                    <input :ref="student['id'] + '-id'" :value="student['id']"
+                        v-bind:disabled="!(enabledId == student['id'])" />
+                </td>
+                <td>
+                    <input :ref="student['id'] + '-course'" :value="student['course']"
+                        v-bind:disabled="!(enabledId == student['id'])" />
+                </td>
+                <td>
+                    <button class="fa fa-floppy-o" @click="updateById(student['id'])"
+                        v-bind:disabled="!(enabledId == student['id'])"></button>
+
+                    <button class="fa fa-pencil-square-o" @click="enableEdit(student['id'])"
+                        v-bind:disabled="(enabledId == student['id'])"></button>
+
+                    <button class="fa fa-trash-o" @click="deleteById(student['id'])"
+                        v-bind:disabled="(enabledId == student['id'])"></button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <style scoped>
 table {
-    width: 100%;
+    width: 100;
 }
 </style>
